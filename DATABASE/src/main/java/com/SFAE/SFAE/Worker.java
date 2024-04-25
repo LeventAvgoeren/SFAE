9622821181aad1f88131dac6f2129ac73b31a0f5
Levent Avgoeren <s90643@bht-berlin.de>
2024-04-25 18:58:04 +0200

    Worker Class implemented

--- DATABASE/src/main/java/com/SFAE/SFAE/Worker.java
+++ DATABASE/src/main/java/com/SFAE/SFAE/Worker.java
@@ -2,140 +2,31 @@ package com.SFAE.SFAE;
 
 import org.springframework.stereotype.Component;
 
-@Component
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Worker")
+
 public class Worker {
 
-Long id;
-String name;
-String location;
-String password;
-Status status;
-StartusOrder startusOrder;
-Float range;
-JobList jobType;
-Float minPayment;
-Double rating;
-Boolean veification;
-
-public Worker(Long id, String name, String location, String password, Status status, StartusOrder startusOrder,
-Float range, JobList jobType, Float minPayment, Double rating, Boolean veification) {
-  this.id = id;
-  this.name = name;
-  this.location = location;
-  this.password = password;
-  this.status = status;
-  this.startusOrder = startusOrder;
-  this.range = range;
-  this.jobType = jobType;
-  this.minPayment = minPayment;
-  this.rating = rating;
-  this.veification = veification;
-}
+@Id
+private Long id;
+private String name;
+private String location;
+private String password;
+private Status status;
+private StartusOrder startusOrder;
+private Float range;
+private JobList jobType;
+private Float minPayment;
+private Double rating;
+private Boolean veification;
 
 
-public Long getId() {
-  return id;
-}
-
-
-public String getName() {
-  return name;
-}
-
-
-public void setName(String name) {
-  this.name = name;
-}
-
-
-public String getLocation() {
-  return location;
-}
-
-
-public void setLocation(String location) {
-  this.location = location;
-}
-
-
-public String getPassword() {
-  return password;
-}
-
-
-public void updatePassword(String password) {
-  this.password = password;
-}
-
-
-public Status getStatus() {
-  return status;
-}
-
-
-public void setStatus(Status status) {
-  this.status = status;
-}
-
-
-public StartusOrder getStartusOrder() {
-  return startusOrder;
-}
-
-
-public void setStartusOrder(StartusOrder startusOrder) {
-  this.startusOrder = startusOrder;
-}
-
-
-public Float getRange() {
-  return range;
-}
-
-
-public void setRange(Float range) {
-  this.range = range;
-}
-
-
-public JobList getJobType() {
-  return jobType;
-}
-
-
-public void setJobType(JobList jobType) {
-  this.jobType = jobType;
-}
-
-
-public Float getMinPayment() {
-  return minPayment;
-}
-
-
-public void setMinPayment(Float minPayment) {
-  this.minPayment = minPayment;
-}
-
-
-public Double getRating() {
-  return rating;
-}
-
-
-public void setRating(Double rating) {
-  this.rating = rating;
-}
-
-
-public Boolean getVeification() {
-  return veification;
-}
-
-
-public void setVeification(Boolean veification) {
-  this.veification = veification;
-}
 
 
 

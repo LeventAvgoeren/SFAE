289af3e558dc43cf1e4df054e0c690bdc9cc3542
Levent Avgoeren <s90643@bht-berlin.de>
2024-04-26 14:32:39 +0200

    Order Entity created, getter and setter Created

---
+++ DATABASE/src/main/java/com/SFAE/SFAE/Entity/Order.java
@@ -0,0 +1,115 @@
+package com.SFAE.SFAE.Entity;
+
+import com.SFAE.SFAE.ENUM.JobList;
+import com.SFAE.SFAE.ENUM.StartusOrder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Data
+@Entity
+@Table(name="Order")
+
+public class Order {
+
+  @Id
+  @Column(name = "id")
+  private String id;
+  @Column(name = "jobtype")
+  private JobList jobtype;
+  @Column(name = "adress")
+  private String adress;
+  @Column(name = "payment")
+  private Float payment;
+  @Column(name = "status")
+  private StartusOrder status;
+  @Column(name = "range")
+  private Float range;
+  @Column(name = "workerID")
+  private Worker worker;
+  @Column(name = "customerID")
+  private Customer customer;
+
+  public Order(String id, JobList jobtype, String adress, Float payment, StartusOrder status, Float range,
+      Worker worker, Customer customer) {
+    this.id = id;
+    this.jobtype = jobtype;
+    this.adress = adress;
+    this.payment = payment;
+    this.status = status;
+    this.range = range;
+    this.worker = worker;
+    this.customer = customer;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public JobList getJobtype() {
+    return jobtype;
+  }
+
+  public void setJobtype(JobList jobtype) {
+    this.jobtype = jobtype;
+  }
+
+  public String getAdress() {
+    return adress;
+  }
+
+  public void setAdress(String adress) {
+    this.adress = adress;
+  }
+
+  public Float getPayment() {
+    return payment;
+  }
+
+  public void setPayment(Float payment) {
+    this.payment = payment;
+  }
+
+  public StartusOrder getStatus() {
+    return status;
+  }
+
+  public void setStatus(StartusOrder status) {
+    this.status = status;
+  }
+
+  public Float getRange() {
+    return range;
+  }
+
+  public void setRange(Float range) {
+    this.range = range;
+  }
+
+  public Worker getWorker() {
+    return worker;
+  }
+
+  public void setWorker(Worker worker) {
+    this.worker = worker;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  @Override
+  public String toString() {
+    return "Order [id=" + id + ", jobtype=" + jobtype + ", adress=" + adress + ", payment=" + payment + ", status="
+        + status + ", range=" + range + ", worker=" + worker + ", customer=" + customer + "]";
+  }
+}

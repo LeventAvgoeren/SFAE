0f4a06597d9186464965c27f48742ee2ae59d15a
ducdai1 <duc.d.nguyen@campus.tu-berlin.de>
2024-04-26 16:32:18 +0200

    Registration Site finished

--- mein-app-name/src/components/PageRegistration.tsx
+++ mein-app-name/src/components/PageRegistration.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './DesignVorlage.css';
+import { Button, Col, Row } from 'react-bootstrap';
+import './PageRegistration.css';
 
 export function PageRegistration() {
+
     return (
         <div className="background">
             <div className="container-frame">
 
             <div style={{ height: '300px' }}></div>
-
+            <img src={'/SFAE_Logo.png'} alt="SFAE Logo" className="img-fluid" />
+            <h1 className="text-center">Registrieren</h1>
+            <div style={{ height: '20px' }}></div>
 
             <div className="input-group mb-3">
-                
 
-    
-  <label className="input-group-text" htmlFor="inputGroupSelect01">Options</label>
-  <select className="form-select" id="inputGroupSelect01">
-    <option selected>Choose...</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-  </select>
+                
+    </div>
+
+
+<Row className="mb-3">
+  <Col>
+    <div className="d-flex flex-row">
+      <input type="text" className="form-control me-2" id="inputName1" placeholder="Vorname"/>
+      <input type="text" className="form-control" id="inputName2" placeholder="Nachname"/>
+    </div>
+  </Col>
+</Row>
+
+<div className="input-group mb-3">
+  <input type="text" className="form-control" id="inputAddress1" placeholder="Addresse" />
 </div>
 
+<div className="input-group mb-3">
+  <input type="text" className="form-control" id="inputAddress1" placeholder="Email" />
+</div>
+
+<div className="input-group mb-3">
+  <input type="password" className="form-control" id="password" placeholder="Password" />
+</div>
+
+{/* 
 <div className="input-group mb-3">
   <select className="form-select" id="inputGroupSelect02">
     <option selected>Choose...</option>
@@ -31,27 +51,21 @@ export function PageRegistration() {
     <option value="3">Three</option>
   </select>
   <label className="input-group-text" htmlFor="inputGroupSelect02">Options</label>
-</div>
+</div> */}
 
-<div className="input-group mb-3">
-  <button className="btn btn-outline-secondary" type="button">Button</button>
-  <select className="form-select" id="inputGroupSelect03" aria-label="Example select with button addon">
-    <option selected>Choose...</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-  </select>
-</div>
 
-<div className="input-group">
-  <select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-    <option selected>Choose...</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-  </select>
-  <button className="btn btn-outline-secondary" type="button">Button</button>
-</div>
+{/* anmelden abbrechen button */}
+  <Row>
+                      <Col xs="6">                      
+                        <Button color="primary" className="px-4">
+                            Abbrechen
+                         </Button>
+                      </Col>
+                      <Col xs="6" className="text-right">
+                        <Button color="link" className="px-4">Anmelden</Button>
+                      </Col>
+                    </Row>
+
 <div style={{ height: '400px' }}></div>
 
             </div>

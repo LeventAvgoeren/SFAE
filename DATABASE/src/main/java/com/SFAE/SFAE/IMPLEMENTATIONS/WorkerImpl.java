0ada79f28ab82e92c70863415e3844f842342383
Levent Avgoeren <s90643@bht-berlin.de>
2024-04-26 17:24:02 +0200

    WorkerImpl implemented

---
+++ DATABASE/src/main/java/com/SFAE/SFAE/IMPLEMENTATIONS/WorkerImpl.java
@@ -0,0 +1,137 @@
+package com.SFAE.SFAE.IMPLEMENTATIONS;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+import com.SFAE.SFAE.ENTITY.Worker;
+import com.SFAE.SFAE.INTERFACE.WorkerInterface;
+/**
+ * @author Levent
+ */
+
+public class WorkerImpl implements WorkerInterface {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  private DataFactoryImp dataFactory;
+
+  @Override
+  public long countWorker() {
+    List<Object> result = jdbcTemplate.query(
+
+        "SELECT COUNT(ID) FROM CUSTOMER",
+
+        (rs, rowNum) -> {
+          long count = rs.getInt(1);
+          return count;
+        });
+    if (result.isEmpty()) {
+      return 0;
+    } else {
+      return result.size();
+    }
+  }
+
+  @Override
+  public Iterable<Worker> findAllWorker() {
+
+    var result = jdbcTemplate.queryForStream(
+
+        "SELECT * FROM Worker",
+
+        (rs, rowNum) -> {
+
+          long id = rs.getLong("id");
+          String name = rs.getString("name");
+          String location = rs.getString("location");
+          String email = rs.getString("email");
+          String status = rs.getString("status");
+          String statusOrder = rs.getString("statusOrder");
+          Float range = rs.getFloat("range");
+          Float jobType = rs.getFloat("jobType");
+          String minPayment = rs.getString("minPayment");
+          Double rating = rs.getDouble("rating");
+          Boolean verification = rs.getBoolean("verification");
+
+          return dataFactory.createWorker(id, name, location, location, email, status, range, minPayment, statusOrder,
+              jobType, rating, verification);
+        })
+        .filter(opt -> opt.isPresent())
+        .map(opt -> opt.get())
+        .collect(Collectors.toList());
+    return result;
+  }
+
+  @Override
+  public Optional<Worker> findWorkersbyID(long id) {
+
+    List<Optional<Worker>> result = jdbcTemplate.query(
+
+        "SELECT * FROM WORKER WHERE ID = ?",
+        // Damit setzt man das frage zeichen auf die erste stelle des platzhalters ?
+        ps -> {
+
+          ps.setLong(1, (long) id);
+        },
+
+        (rs, rowNum) -> {
+          /*
+           * Extract values from ResultSet.
+           */
+
+          String name = rs.getString("name");
+          String location = rs.getString("location");
+          String email = rs.getString("email");
+          String status = rs.getString("status");
+          String statusOrder = rs.getString("statusOrder");
+          Float range = rs.getFloat("range");
+          Float jobType = rs.getFloat("jobType");
+          String minPayment = rs.getString("minPayment");
+          Double rating = rs.getDouble("rating");
+          Boolean verification = rs.getBoolean("verification");
+
+          return dataFactory.createWorker(id, name, location, location, email, status, range, minPayment, statusOrder,
+              jobType, rating, verification);
+        });
+    return result.size() > 0 ? result.get(0) : Optional.empty();
+
+  }
+
+  @Override
+  public Optional<Worker> findWorkerbyName(String name) {
+    List<Optional<Worker>> result = jdbcTemplate.query(
+        "SELECT * FROM CUSTOMER WHERE NAME LIKE ?",
+        ps -> {
+          ps.setString(1, "%" + name + "%");
+        },
+        (rs, rowNum) -> {
+
+          long id = rs.getLong("id");
+          String location = rs.getString("location");
+          String email = rs.getString("email");
+          String status = rs.getString("status");
+          String statusOrder = rs.getString("statusOrder");
+          Float range = rs.getFloat("range");
+          Float jobType = rs.getFloat("jobType");
+          String minPayment = rs.getString("minPayment");
+          Double rating = rs.getDouble("rating");
+          Boolean verification = rs.getBoolean("verification");
+
+          return dataFactory.createWorker(id, name, location, location, email, status, range, minPayment, statusOrder,
+              jobType, rating, verification);
+
+        });
+    if (!result.isEmpty()) {
+      return result.get(0);
+    } else {
+      return null;
+    }
+  }
+}
--- DATABASE/src/main/java/com/SFAE/SFAE/Interface/WorkerInterface.java
+++ DATABASE/src/main/java/com/SFAE/SFAE/Interface/WorkerInterface.java
@@ -1,11 +1,19 @@
 package com.SFAE.SFAE.INTERFACE;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.SFAE.SFAE.ENTITY.Worker;
 
 @RepositoryRestResource
-public interface WorkerInterface extends JpaRepository<Worker, Long> {
-    
+public interface WorkerInterface {
+
+  long countWorker();
+
+  Iterable<Worker> findAllWorker();
+
+  Optional<Worker> findWorkersbyID(long id);
+
+  Optional<Worker> findWorkerbyName(String Name);
 }

ebf28cf47293f0b7751612f1eb337ce26b71a732
Levent Avgoeren <s90643@bht-berlin.de>
2024-04-29 22:52:07 +0200

    test

--- DATABASE/src/main/java/com/SFAE/SFAE/Service/Authentication.java
+++ DATABASE/src/main/java/com/SFAE/SFAE/Service/Authentication.java
@@ -3,16 +3,9 @@ package com.SFAE.SFAE.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.SFAE.SFAE.ENTITY.Customer;
 import com.SFAE.SFAE.ENTITY.Worker;
-import com.SFAE.SFAE.IMPLEMENTATIONS.CustomerImp;
 import com.SFAE.SFAE.INTERFACE.CustomerInterface;
 import com.SFAE.SFAE.INTERFACE.WorkerInterface;
-import com.SFAE.SFAE.Security.JWT;
-
-/**
- * @author erayzor
- */
 
 @Component
 public class Authentication {
@@ -22,41 +15,26 @@ public class Authentication {
     @Autowired
     private PasswordHasher pw;
 
-    @Autowired
-    CustomerImp cus;
 
-    @Autowired
-    PasswordHasher encoder;
+    public boolean loginCustomer(String EMail, String Password){
 
-    @Autowired
-    JWT jwt;
-
-    public String loginCustomer(String EMail, String Password){
-        try{
-            Customer foundCustomer = cus.findEmail(EMail);
-            if(foundCustomer instanceof Customer){
-                        if(encoder.comparePassword(foundCustomer.getPassword(), Password)){
-                                return jwt.verifyPasswordAndCreateJWT(EMail, Password);
-                        };
-                        }
-
-        } catch(Exception e){
-                System.out.println(e);
-        }
-        
-        return null;
+        return false;
     }
+
+
 
 
     
     public boolean loginWorker(String email, String password){
         if(email==null || password==null){
-            throw new IllegalArgumentException("Enter email or password")
+            throw new IllegalArgumentException("Enter emails or passwords");
         }
          Iterable <Worker> workers=dao.findAllWorker();
 
          for (Worker worker : workers) {
-            if(worker.getEmail().equals(email) && comparePassword())
+            if(worker.getEmail().equals(email) && pw.comparePassword(worker.getPassword(), password)){
+                return true;
+            }
          }
         
 

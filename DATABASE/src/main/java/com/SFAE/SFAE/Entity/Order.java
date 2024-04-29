e7de5fa29bdd97b9035cd0469ac13cb7dafb990c
ErayZor <erayzot045@gmail.com>
2024-04-29 15:27:06 +0200

    HTTPS Backend upload

---
+++ DATABASE/src/main/java/com/SFAE/SFAE/Entity/Order.java
@@ -0,0 +1,1 @@
+package com.SFAE.SFAE.ENTITY;
--- mein-app-name/src/backend/api.ts
+++ mein-app-name/src/backend/api.ts
@@ -3,7 +3,7 @@ import { fetchWithErrorHandling } from "./fetchWithErrorHandling";
 
 export async function getCustomerByName(name: String): Promise<any> {
 
-    const url = "https://localhost:8443" + `/customer/usr/${name}`;
+    const url =  process.env.REACT_APP_API_SERVER_URL + `/customer/usr/${name}`;
     const response = await fetchWithErrorHandling(url,{credentials: 'include' as RequestCredentials})
     
     return await response;

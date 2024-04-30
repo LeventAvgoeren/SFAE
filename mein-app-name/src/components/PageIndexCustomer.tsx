35e80dbaee34ad30e2a45e31725275380001c819
ducdai1 <duc.d.nguyen@campus.tu-berlin.de>
2024-04-30 19:35:11 +0200

    relative path for pictures and gifs in Pageindex

--- mein-app-name/src/components/PageIndexCustomer.tsx
+++ mein-app-name/src/components/PageIndexCustomer.tsx
@@ -2,31 +2,19 @@ import { NavDropdown } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './PageIndexCustomer.css'; 
+import './PageIndexCustomer.css';
 
 
 export function PageIndexCustomer() {
   return (
+
+    // Navbar 
     <>
       <Navbar bg="primary" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={"/SFAE_Logo.png"}
-              height="65"
-              className="d-inline-block align-top"
-              alt="SFAE Logo"
-            />
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            </Nav>
 
-            <Nav>
-            <Nav.Link href="#home">Chat</Nav.Link>
-            <Nav.Link href="#features">Finanzen</Nav.Link>
-            <Nav.Link href="#pricing">Support</Nav.Link>
-
-          <NavDropdown title={<img src={"/logo192.png"} height="35" alt="Dropdown Logo" />} id="collapsible-nav-dropdown">
+          <Nav>
+            <NavDropdown title={<img src={"/logo192.png"} height="35" alt="Dropdown Logo" />} id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -37,10 +25,31 @@ export function PageIndexCustomer() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#home">Chat</Nav.Link>
+            <Nav.Link href="#features">Finanzen</Nav.Link>
+            <Nav.Link href="#pricing">Support</Nav.Link>
+
           </Nav>
+
+          <Nav className="me-auto">
+          </Nav>
+
+          <Navbar.Brand href="/">
+            <img
+              src={"/SFAE_Logo.png"}
+              height="65"
+              className="d-inline-block align-top"
+              alt="SFAE Logo"
+            />
+          </Navbar.Brand>
+
         </Container>
       </Navbar>
+      
+      
     </>
+
+    
 
   );
 }

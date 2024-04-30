499d9980bea9e24ac1db0153608b80dba72a0419
ErayZor <erayzot045@gmail.com>
2024-04-30 20:52:13 +0200

    Auth fix

--- DATABASE/src/main/java/com/SFAE/SFAE/Service/Authentication.java
+++ DATABASE/src/main/java/com/SFAE/SFAE/Service/Authentication.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.SFAE.SFAE.ENTITY.Customer;
-import com.SFAE.SFAE.ENTITY.Worker;
 import com.SFAE.SFAE.IMPLEMENTATIONS.CustomerImp;
-import com.SFAE.SFAE.INTERFACE.WorkerInterface;
 import com.SFAE.SFAE.Security.JWT;
 
 
@@ -20,9 +18,6 @@ public class Authentication {
 
     @Autowired
     private CustomerImp cus;
-
-    @Autowired
-    private PasswordHasher encoder;
 
     @Autowired
     private JWT jwt;

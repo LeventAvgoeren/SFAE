939e901dadb9b0f2971f658595fe98dfe1a92102
ErayZor <erayzot045@gmail.com>
2024-05-06 16:30:12 +0200

    added updateContract and deleteContract

--- DATABASE/src/main/java/com/SFAE/SFAE/IMPLEMENTATIONS/ContractImpl.java
+++ DATABASE/src/main/java/com/SFAE/SFAE/IMPLEMENTATIONS/ContractImpl.java
@@ -1,9 +1,18 @@
 package com.SFAE.SFAE.IMPLEMENTATIONS;
 
+import java.sql.PreparedStatement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.SFAE.SFAE.ENTITY.Contract;
 import com.SFAE.SFAE.INTERFACE.ContractInterface;
 
 public class ContractImpl implements ContractInterface {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
   @Override
   public Contract getContract(long id) {
@@ -13,14 +22,46 @@ public class ContractImpl implements ContractInterface {
 
   @Override
   public Contract updateContract(Contract contract) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'updateContract'");
+    int result = jdbcTemplate.update(
+      "UPDATE contract SET jobType = ?, adress = ?, payment = ?, description = ?, statusOrder = ?, range = ? WHERE id = ?",
+      ps -> {
+          ps.setString(1, contract.getJobType().toString());
+          ps.setString(2, contract.getAdress());
+          ps.setString(3, contract.getPayment().toString());
+          ps.setString(4, contract.getDescription());
+          ps.setString(5, contract.getStatusOrder().toString());
+          ps.setDouble(6, contract.getRange());
+          ps.setLong(7, contract.getId());
+      });
+
+    if (result > 0) {
+      return getContract(Long.valueOf(contract.getId()));
+    }
+
+    return null;
   }
 
   @Override
   public boolean deleteContract(long id) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'deleteContract'");
+      if(id < 0){
+        throw new IllegalArgumentException("Wrong Id: " + id);
+      }
+
+      try{
+          int deleted = jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection
+                        .prepareStatement("DELETE FROM CONTRACT WHERE ID = ?;");
+                ps.setInt(1, (int) id);
+                return ps;
+            });
+
+            if (deleted != 1) {
+                throw new IllegalArgumentException("Id could not been deleted");
+            }
+            return true;
+      } catch (DataAccessException dax){
+        return false;
+      }
   }
 
   @Override
---
+++ DATABASE/src/main/java/com/SFAE/SFAE/MVC/WebConfig.java
@@ -0,0 +1,20 @@
+
+package com.SFAE.SFAE.MVC;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+      @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+    
+}

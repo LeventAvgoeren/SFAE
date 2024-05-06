ecc886aff93a6dd3551b4bd620d5247a57965680
Levent Avgoeren <s90643@bht-berlin.de>
2024-05-06 16:15:01 +0200

    Implements Contract interface

---
+++ DATABASE/src/main/java/com/SFAE/SFAE/Entity/Contract.java
@@ -0,0 +1,141 @@
+package com.SFAE.SFAE.ENTITY;
+
+import com.SFAE.SFAE.ENUM.JobList;
+import com.SFAE.SFAE.ENUM.Payment;
+import com.SFAE.SFAE.ENUM.StatusOrder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "contract")
+public class Contract {
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "jobType")
+  @Enumerated(EnumType.STRING)
+  private JobList jobType;
+
+  @Column(name = "adress")
+  private String adress;
+
+  @Column(name = "payment")
+  @Enumerated(EnumType.STRING)
+  private Payment payment;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "statusOrder")
+  @Enumerated(EnumType.STRING)
+  private StatusOrder statusOrder;
+
+  @Column(name = "range")
+  private Double range;
+
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  private Customer customer;
+
+  @ManyToOne
+  @JoinColumn(name = "worker_id")
+  private Worker worker;
+
+
+  public Contract(JobList jobType, String adress, Payment payment, String description, StatusOrder statusOrder,
+      Double range, Customer customer, Worker worker) {
+    this.jobType = jobType;
+    this.adress = adress;
+    this.payment = payment;
+    this.description = description;
+    this.statusOrder = statusOrder;
+    this.range = range;
+    this.customer = customer;
+    this.worker = worker;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public JobList getJobType() {
+    return jobType;
+  }
+
+  public void setJobType(JobList jobType) {
+    this.jobType = jobType;
+  }
+
+  public String getAdress() {
+    return adress;
+  }
+
+  public void setAdress(String adress) {
+    this.adress = adress;
+  }
+
+  public Payment getPayment() {
+    return payment;
+  }
+
+  public void setPayment(Payment payment) {
+    this.payment = payment;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public StatusOrder getStatusOrder() {
+    return statusOrder;
+  }
+
+  public void setStatusOrder(StatusOrder statusOrder) {
+    this.statusOrder = statusOrder;
+  }
+
+  public Double getRange() {
+    return range;
+  }
+
+  public void setRange(Double range) {
+    this.range = range;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public Worker getWorker() {
+    return worker;
+  }
+
+  public void setWorker(Worker worker) {
+    this.worker = worker;
+  }
+}
---
+++ DATABASE/src/main/java/com/SFAE/SFAE/IMPLEMENTATIONS/ContractImpl.java
@@ -0,0 +1,32 @@
+package com.SFAE.SFAE.IMPLEMENTATIONS;
+
+import com.SFAE.SFAE.ENTITY.Contract;
+import com.SFAE.SFAE.INTERFACE.ContractInterface;
+
+public class ContractImpl implements ContractInterface {
+
+  @Override
+  public Contract getContract(long id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getContract'");
+  }
+
+  @Override
+  public Contract updateContract(Contract contract) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateContract'");
+  }
+
+  @Override
+  public boolean deleteContract(long id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteContract'");
+  }
+
+  @Override
+  public Contract createContract(Contract contract) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createContract'");
+  }
+  
+}

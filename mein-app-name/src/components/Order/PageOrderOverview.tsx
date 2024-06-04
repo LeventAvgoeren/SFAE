2181730e02f27a54bf581df5edb4b45a034dd089
Your Name <name@domain.example>
2024-06-04 18:24:47 +0200

    Design

--- mein-app-name/src/components/Order/PageOrderOverview.tsx
+++ mein-app-name/src/components/Order/PageOrderOverview.tsx
@@ -109,7 +109,7 @@ export function PageOrderOverview() {
       }
     }
     toggleShow(); // Schließt das Modal
-  }; 
+  };
 
   const handleCancelConfirm = async () => {
     if (conData && conData.worker && conData.worker.id) {
@@ -175,7 +175,7 @@ export function PageOrderOverview() {
 
           map.attributionControl.setPrefix(false);
           map.attributionControl.remove();
-          map.on('click', function(e) {
+          map.on('click', function (e) {
             e.originalEvent.preventDefault();
             e.originalEvent.stopPropagation();
           });
@@ -187,14 +187,14 @@ export function PageOrderOverview() {
               L.latLng(workerCoords.latitude, workerCoords.longitude)
             ],
             routeWhileDragging: false,
-            createMarker: function() { return null; } 
-          } as any).addTo(map); 
+            createMarker: function () { return null; }
+          } as any).addTo(map);
 
           // Füge benutzerdefinierte Icons hinzu
           L.marker([customerCoords.latitude, customerCoords.longitude], { icon: customIconCustomer }).addTo(map);
           L.marker([workerCoords.latitude, workerCoords.longitude], { icon: customIconWorker }).addTo(map);
 
-          control.on('routesfound', function(e) {
+          control.on('routesfound', function (e) {
             const routes = e.routes;
             const summary = routes[0].summary;
             const totalTimeMinutes = Math.round(summary.totalTime / 60); // Sekunden in Minuten umrechnen
@@ -211,7 +211,7 @@ export function PageOrderOverview() {
     }
   }, [conData]);
 
-  
+
 
   if (!contractData.length) {
     return <div className="Backg">No contracts found</div>;
@@ -232,7 +232,7 @@ export function PageOrderOverview() {
 
   return (
     <>
-    
+
       <div className="Backg">
         <NavbarComponent />
         {loading || !workerAssigned ? (
@@ -242,133 +242,139 @@ export function PageOrderOverview() {
           </div>
         ) : (
           <div className="containertest">
-            <h1>Order Information</h1> 
-         
+            <h1>Order Information</h1>
+
             <div className="d-flex justify-content-between align-items-center py-3">
               <h2 className="h5 mb-0" style={{ color: "white" }}>Order ID: <span className="fw-bold text-body white-text">{conData.id}</span></h2>
             </div>
             <div className="row">
-              <div className="col-lg-8 glassmorphism">   
-            
-                <div className="mb-3 d-flex justify-content-between"> 
-                    <span className="badge rounded-pill bg-info" style={{ marginTop: "10%" }}>DIENSTLEISTUNG: {conData.jobType}</span>
-                  <div className="d-flex">
+              <div className="col-lg-8 glassmorphism">
+
+                <div className="badge-container glassmorphism">
+                  <div>
+                    <div>Dienstleistung: {conData.jobType}</div>
+                    <div className="info-item">Distanz: {routeDistance}</div>
+                    <div className="payment-info">
+                      <span>Betrag:</span>
+                      <span className="info-item">{conData.maxPayment}€</span>
+                    </div>
+                    <div className="info-item">Dauer: {routeTime}</div>
                   </div>
-                </div> 
-                <div style={{ justifyItems:"center", alignContent:"center"}}>
+                <div className="d-flex">
+                </div>
+              </div>
+              <div style={{ justifyItems: "center", alignContent: "center" }}>
                 <main style={{ gridArea: 'map', display: 'flex', alignItems: 'center', width: '100%', height: '100%', borderRadius: "50%" }} draggable="false">
-                  <div id="map" style={{ width: '100%', height: '200px' }}></div>
+                  <div id="map" style={{ borderRadius: "28px", width: '100%', height: '450px' }}></div>
                 </main>
 
-                <p style={{color:"white"}}>Dauer: {routeTime}</p>
-                <p style={{color:"white"}}>Distanz: {routeDistance}</p>
-                </div>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="d-flex align-items-center mb-2">
-                        <div className="flex-shrink-0">
-                          <img src={foto} width="45" className="img-fluid" alt="" style={{ borderRadius: "20%" }} />
-                        </div>
-                        <div className="flex-lg-grow-1 ms-3">
-                        </div>
-                       
-                        <td style={{ marginLeft: "80%", color: "white" }}>Betrag:</td>
-                        <td style={{ color: "white" }}>{conData.maxPayment}€</td>
+              </div>
+              <tbody>
+                <tr>
+                  <td>
+                   
+                  </td>
+                </tr>
+              </tbody>
+              <div className="d-flex justify-content-between">
+                {conData.statusOrder === "ACCEPTED" && <button onClick={toggleShow} className="btn btn-danger mb-4" style={{ width: "250px", marginLeft: "5%", marginTop: "20%" }}>
+                  Auftrag beendet
+                </button>}
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className="card mb-4 glassmorphism" style={{ overflowX: 'hidden' }}>
+                <div className="info-section">
+                  <h4>Customer Beschreibung: </h4>
+                  <h4>{conData.description}</h4>
+                  
+                  <div className="d-flex align-items-center mb-2">
+                      <div className="flex-shrink-0">
+                        <img src={foto} width="25" className="img-fluid" alt="" style={{ borderRadius: "20%" }} />
                       </div>
-                    </td>
-                  </tr>
-                </tbody>
-                <div className="d-flex justify-content-between">
-                  {conData.statusOrder === "ACCEPTED" && <button onClick={toggleShow} className="btn btn-danger mb-4" style={{ width: "250px", marginLeft: "5%", marginTop: "20%" }}>
-                    Auftrag beendet
-                  </button>}
+                      <div className="flex-lg-grow-1 ms-3">
+                      </div>
+                    </div>
+
                 </div>
               </div>
-
-              <div className="col-lg-4">
-                <div className="card mb-4 glassmorphism">
-                  <div className="info-section">
-                    <h4>Customer Beschreibung: </h4>
-                    <p>{conData.description}</p>
-                  </div>
-                </div>
-                <div className="card mb-4 glassmorphism">
-                  <div className="details-panel">
-                    <h4>Order Details</h4>
-                    <p className="text-muted" style={{ color: "white" }}>
-                      Order ID: <span className="fw-bold text-body  white-text" style={{ color: "white" }}>{conData.id}</span>
-                    </p>
-                    <p className="text-muted" style={{ color: "white" }}>
-                      Umkreis des Workers <span className="fw-bold text-body white-text" style={{ color: "white" }}>: {conData.range} km</span>
-                    </p>
-                    <p className="text-muted">Job Type: {conData.jobType}</p>
-                    <p className="text-muted">Status deiner Bestellung: {conData.statusOrder}</p>
-                    <hr />
-                    <h3 className="h6">Worker Details</h3>
-                    {conData.worker && (
-                      <>  <div className="Foto">
-                            <img
-                              src={workerFoto}
-                              width="45"
-                              className="img-fluid"
-                              alt=""
-                              style={{ borderRadius: "20%" }}
-                            />
-                          </div>
-                        <address>
-                          <strong>Name: {conData.worker.name}</strong><br />
-                          Email: {conData.worker.email}<br />
-                          Adresse: {conData.worker.location}<br />
-                        </address>
-                      </>
-                    )}
-                  </div>
+              <div className="card mb-4 glassmorphism">
+                <div className="details-panel">
+                  <h4>Order Details</h4>
+                  <p className="text-muted" style={{ color: "white" }}>
+                    Order ID: <span className="fw-bold text-body  white-text" style={{ color: "white" }}>{conData.id}</span>
+                  </p>
+                  <p className="text-muted" style={{ color: "white" }}>
+                    Umkreis des Workers <span className="fw-bold text-body white-text" style={{ color: "white" }}>: {conData.range} km</span>
+                  </p>
+                  <p className="text-muted">Job Type: {conData.jobType}</p>
+                  <p className="text-muted">Status deiner Bestellung: {conData.statusOrder}</p>
+                  <hr />
+                  <h3 className="h6">Worker Details</h3>
+                  {conData.worker && (
+                    <>  <div className="Foto">
+                      <img
+                        src={workerFoto}
+                        width="45"
+                        className="img-fluid"
+                        alt=""
+                        style={{ borderRadius: "20%" }}
+                      />
+                    </div>
+                      <address>
+                        <strong>Name: {conData.worker.name}</strong><br />
+                        Email: {conData.worker.email}<br />
+                        Adresse: {conData.worker.location}<br />
+                      </address>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
+          </div>
           </div>
         )}
-        <div className={`modal fade ${modalShow ? 'show' : ''}`} style={{ display: modalShow ? 'block' : 'none' }}>
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Auftrag beendet</h5>
-              </div>
-              <div className="modal-body">
-                Bist du sicher, dass du diesen Auftrag als beendet markieren möchtest? Wurde alles ordnungsgemäß ausgeführt?
-              </div>
-              <div className="modal-footer">
-                <Row style={{ gap: "12px" }}>
-                  <button type="button" className="btn btn-secondary" onClick={toggleShow} style={{ width: "150px" }}>Abbrechen</button>
-                  <button type="button" className="btn btn-primary" style={{ width: "150px", gap: "12" }} onClick={handleConfirm}>Bestätigen</button>
-                </Row>
-              </div>
+      <div className={`modal fade ${modalShow ? 'show' : ''}`} style={{ display: modalShow ? 'block' : 'none' }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Auftrag beendet</h5>
+            </div>
+            <div className="modal-body">
+              Bist du sicher, dass du diesen Auftrag als beendet markieren möchtest? Wurde alles ordnungsgemäß ausgeführt?
+            </div>
+            <div className="modal-footer">
+              <Row style={{ gap: "12px" }}>
+                <button type="button" className="btn btn-secondary" onClick={toggleShow} style={{ width: "150px" }}>Abbrechen</button>
+                <button type="button" className="btn btn-primary" style={{ width: "150px", gap: "12" }} onClick={handleConfirm}>Bestätigen</button>
+              </Row>
             </div>
           </div>
         </div>
-        {modalShow && <div className="modal-backdrop fade show"></div>}
-
-        <div className={`modal fade ${cancelModalShow ? 'show' : ''}`} style={{ display: cancelModalShow ? 'block' : 'none' }}>
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Auftrag stornieren</h5>
-              </div>
-              <div className="modal-body" style={{ gap: "12px" }}>
-                Bist du sicher, dass du diesen Auftrag stornieren möchtest?
-              </div>
-              <div className="modal-footer">
-                <Row>
-                  <button type="button" className="btn btn-secondary" onClick={toggleCancelShow} style={{ width: "150px", gap: "12px" }}>Abbrechen</button>
-                  <button type="button" className="btn btn-warning" style={{ width: "150px", gap: "12px" }} onClick={handleCancelConfirm}>Bestätigen</button>
-                </Row>
-              </div>
-            </div>
-          </div>
-        </div>
-        {cancelModalShow && <div className="modal-backdrop fade show"></div>}
       </div>
+      {modalShow && <div className="modal-backdrop fade show"></div>}
+
+      <div className={`modal fade ${cancelModalShow ? 'show' : ''}`} style={{ display: cancelModalShow ? 'block' : 'none' }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Auftrag stornieren</h5>
+            </div>
+            <div className="modal-body" style={{ gap: "12px" }}>
+              Bist du sicher, dass du diesen Auftrag stornieren möchtest?
+            </div>
+            <div className="modal-footer">
+              <Row>
+                <button type="button" className="btn btn-secondary" onClick={toggleCancelShow} style={{ width: "150px", gap: "12px" }}>Abbrechen</button>
+                <button type="button" className="btn btn-warning" style={{ width: "150px", gap: "12px" }} onClick={handleCancelConfirm}>Bestätigen</button>
+              </Row>
+            </div>
+          </div>
+        </div>
+      </div>
+      {cancelModalShow && <div className="modal-backdrop fade show"></div>}
+    </div>
     </>
   );
 }

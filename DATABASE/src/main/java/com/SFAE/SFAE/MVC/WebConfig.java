332d53482fb4aba485e954064de3a329c48fcdaf
Ahmad <s90358@bht-berlin.de>
2024-06-11 10:43:38 +0200

    WebConfig.java

--- DATABASE/src/main/java/com/SFAE/SFAE/MVC/WebConfig.java
+++ DATABASE/src/main/java/com/SFAE/SFAE/MVC/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
       @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("erayzor.de")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .exposedHeaders("Access-Control-Allow-Origin")

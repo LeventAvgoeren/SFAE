782f733d585b27ffdbde706dfaf18533aea580c7
Ahmad <s90358@bht-berlin.de>
2024-06-13 13:09:58 +0200

    Added PNG for Logout and changed PNG for Live-Chat

--- mein-app-name/src/components/navbar/NavbarComponent.tsx
+++ mein-app-name/src/components/navbar/NavbarComponent.tsx
@@ -1,4 +1,3 @@
-
 import { NavDropdown } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -21,53 +20,52 @@ interface Message {
     type?: string; 
 }
 
-
 export function NavbarComponent() {
-  const {loginInfo} = useLoginContext();
+  const { loginInfo } = useLoginContext();
   const [showChat, setShowChat] = useState(false);
-  const params = useParams<{ customerId: string, workerId: string}>();
-  const userId = params.customerId? params.customerId! : params.workerId!;
+  const params = useParams<{ customerId: string, workerId: string }>();
+  const userId = params.customerId ? params.customerId! : params.workerId!;
   const clientRef = useRef<Client | null>(null);
   const [newMessage, setNewMessage] = useState(false);
+
   async function doLogout() {
-      await deleteCookie();
-      window.location.href = "/login";
+    await deleteCookie();
+    window.location.href = "/login";
   }
 
   useEffect(() => {
     const client = new Client({
-        webSocketFactory: () => new SockJS( `${process.env.REACT_APP_API_SERVER_URL}/chat `),
-        reconnectDelay: 5000,
-        heartbeatIncoming: 4000,
-        heartbeatOutgoing: 4000,
-        onConnect: async () => {
-            console.log('Connected');
-            client.subscribe(`/topic/${userId}`, (message: IMessage) => {
-                const receivedMessage: Message = JSON.parse(message.body);
-                if(receivedMessage.content.length > 1){
-                     setNewMessage(true);
-                }
-            });
-        },
-        onStompError: (frame) => {
-            console.error('Broker reported error: ' + frame.headers['message']);
-            console.error('Additional details: ' + frame.body);
-        },
-        onWebSocketClose: (event) => {
-            console.error('WebSocket closed, event:', event);
-        },
+      webSocketFactory: () => new SockJS(`${process.env.REACT_APP_API_SERVER_URL}/chat`),
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: async () => {
+        console.log('Connected');
+        client.subscribe(`/topic/${userId}`, (message: IMessage) => {
+          const receivedMessage: Message = JSON.parse(message.body);
+          if (receivedMessage.content.length > 1) {
+            setNewMessage(true);
+          }
+        });
+      },
+      onStompError: (frame) => {
+        console.error('Broker reported error: ' + frame.headers['message']);
+        console.error('Additional details: ' + frame.body);
+      },
+      onWebSocketClose: (event) => {
+        console.error('WebSocket closed, event:', event);
+      },
     });
 
     client.activate();
     clientRef.current = client;
 
     return () => {
-        if (clientRef.current) {
-            clientRef.current.deactivate();
-        }
+      if (clientRef.current) {
+        clientRef.current.deactivate();
+      }
     };
-}, []);
-
+  }, []);
 
   const toggleChat = () => {
     setShowChat(!showChat);
@@ -79,41 +77,48 @@ export function NavbarComponent() {
   }
 
   return (
-      <>
-          <nav className="page-background">
-    <img src="/Sfae_Logo.png" alt="Logo" style={{height: 100, width: 100}}/>
-              <ul>
-                  {loginInfo && (
-                      <li><a href={`/customer/${loginInfo.userId}`}>Home</a></li>
-                  )}
-                  {loginInfo && (
-                      <li><a href={`/customer/${loginInfo.userId}/profil`}>Profil</a></li>
-                  )}
-                  {loginInfo && (
-                      <li><a href={`/customer/${loginInfo.userId}/uebersicht`}>Übersicht</a></li>
-                  )}
-                  {loginInfo && (
-                      <li><a href={`/customer/${loginInfo.userId}/faq`}>Faq</a></li>
-                  )}
-                   {loginInfo && loginInfo.admin ==="ADMIN" && (
-                      <li><a href={`/admin/${loginInfo.userId}/dienstleistungen`}>Admin</a></li>
-                  )}
-                  <li><a onClick={doLogout}>Abmelden</a></li>
-              </ul>
-              {loginInfo && (
-                <div className="notification-icon" onClick={handleNotificationClick}>
-                    {newMessage && <div className="notification-badge"></div>}
-                    <img src="/icons8-notification-100.png" alt="Benachrichtigungen"/>
-                </div>
-        )}
-      </nav>  
-
-        <div className={`chat-container ${showChat ? 'show' : ''}`}>
-              <ChatComponent onClose={toggleChat} />
+    <>
+      <nav className="page-background">
+        <img src="/Sfae_Logo.png" alt="Logo" style={{ height: 100, width: 100 }} />
+        <ul>
+          {loginInfo && (
+            <li><a href={`/customer/${loginInfo.userId}`}>Home</a></li>
+          )}
+          {loginInfo && (
+            <li><a href={`/customer/${loginInfo.userId}/profil`}>Profil</a></li>
+          )}
+          {loginInfo && (
+            <li><a href={`/customer/${loginInfo.userId}/uebersicht`}>Übersicht</a></li>
+          )}
+          {loginInfo && (
+            <li><a href={`/customer/${loginInfo.userId}/faq`}>Faq</a></li>
+          )}
+          {loginInfo && loginInfo.admin === "ADMIN" && (
+            <li><a href={`/admin/${loginInfo.userId}/dienstleistungen`}>Admin</a></li>
+          )}
+        </ul>
+        {loginInfo && (
+          <div className="icons-container">
+            <div className="icon-item">
+              <a onClick={doLogout}>
+                <img src="/icons8-logout-100.png" alt="Abmelden" className="logout-icon" />
+              </a>
+              <div className="icon-label">Logout</div>
+            </div>
+            <div className="icon-item notification-icon" onClick={handleNotificationClick}>
+              {newMessage && <div className="notification-badge"></div>}
+              <img src="/icons8-chat-64.png" alt="Live-Chat" />
+              <div className="icon-label">Live-Chat</div>
+            </div>
           </div>
-      </>
+        )}
+      </nav>
+
+      <div className={`chat-container ${showChat ? 'show' : ''}`}>
+        <ChatComponent onClose={toggleChat} />
+      </div>
+    </>
   );
 }
-
 
 export default NavbarComponent;

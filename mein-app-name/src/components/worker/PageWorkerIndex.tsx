018247a6220ecc25570367743827134577faeadc
Levent Avgoeren <s90643@bht-berlin.de>
2024-07-10 23:22:09 +0200

    added worker facts in index site

--- mein-app-name/src/components/worker/PageWorkerIndex.tsx
+++ mein-app-name/src/components/worker/PageWorkerIndex.tsx
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
-import LoadingIndicator from '../LoadingIndicator';
-import { Container, Row, Col, Card, Modal, Button } from 'react-bootstrap';
-import './DesignVorlage.css';
-import { WorkerResource, ContractResourceforWorker } from '../../Resources';
-import { getWorkerbyID, getContractByWorkerId, updateWorkerOrderStatus } from '../../backend/api';
-import './PageWorkerIndex.css';
-import NavbarWComponent from './NavbarWComponent';
-import Footer from '../Footer';
-import { MDBBtn } from 'mdb-react-ui-kit';
+import React, { useEffect, useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import LoadingIndicator from "../LoadingIndicator";
+import { Container, Row, Col, Card, Modal, Button } from "react-bootstrap";
+import "./DesignVorlage.css";
+import { WorkerResource, ContractResourceforWorker } from "../../Resources";
+import {
+  getWorkerbyID,
+  getContractByWorkerId,
+  updateWorkerOrderStatus,
+} from "../../backend/api";
+import "./PageWorkerIndex.css";
+import NavbarWComponent from "./NavbarWComponent";
+import Footer from "../Footer";
+import { MDBBtn } from "mdb-react-ui-kit";
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 export function PageWorkerIndex() {
   const { workerId } = useParams<{ workerId?: string }>();
   const [worker, setWorker] = useState<WorkerResource | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [latestContract, setLatestContract] = useState<ContractResourceforWorker | null>(null);
+  const [latestContract, setLatestContract] =
+    useState<ContractResourceforWorker | null>(null);
   const [showModal, setShowModal] = useState(false);
   const [zoom, setZoom] = useState<string | null>(null);
-  const navigate = useNavigate();
+  const [workerJobAnzahl, setWorkerJobAnzahl] =
+    useState<ContractResourceforWorker[]>();
+  const [job, setJob] = useState<String[]>();
+  const [geld, setGeld] = useState(0);
 
+  const navigate = useNavigate();
   useEffect(() => {
     if (!workerId) {
-      setError('Keine Worker ID in der URL gefunden');
+      setError("Keine Worker ID in der URL gefunden");
       setLoading(false);
       return;
     }
@@ -32,7 +42,7 @@ export function PageWorkerIndex() {
         setWorker(workerData);
         setLoading(false);
       } catch (error) {
-        setError('Fehler beim Laden der Daten');
+        setError("Fehler beim Laden der Daten");
         setLoading(false);
       }
     };
@@ -42,21 +52,42 @@ export function PageWorkerIndex() {
   const fetchLatestContract = async () => {
     try {
       const contracts = await getContractByWorkerId(workerId!);
-      console.log('Fetched contracts:', contracts); // Log fetched contracts
+      setWorkerJobAnzahl(contracts);
+
+      if (contracts) {
+        let money = 0;
+        for (let index = 0; index < workerJobAnzahl!.length; index++) {
+          const element = workerJobAnzahl![index];
+          money += element.worker!.minPayment;
+          money = money / workerJobAnzahl!.length;
+        }
+        setGeld(parseFloat(money.toFixed(2)));
+        let jobs: string[] = [];
+        let uniqueJobs = new Set<string>();
+        for (let index = 0; index < workerJobAnzahl!.length; index++) {
+          const element = workerJobAnzahl![index];
+          if (!uniqueJobs.has(element.jobType)) {
+            uniqueJobs.add(element.jobType);
+            jobs.push(element.jobType);
+          }
+          setJob(jobs);
+        }
+      }
+      console.log("Fetched contracts:", contracts); // Log fetched contracts
       if (contracts.length > 0) {
         const latest = contracts.reduce((prev, current) => {
           if (!prev.id || !current.id) {
             return prev;
           }
-          return (prev.id > current.id) ? prev : current;
+          return prev.id > current.id ? prev : current;
         });
-        console.log('Latest contract:', latest); // Log latest contract
+        console.log("Latest contract:", latest); // Log latest contract
         setLatestContract(latest);
       } else {
         setLatestContract(null);
       }
     } catch (error) {
-      console.error('Error fetching contracts', error);
+      console.error("Error fetching contracts", error);
     }
   };
 
@@ -76,13 +107,13 @@ export function PageWorkerIndex() {
   const handleFinishContract = async () => {
     if (workerId) {
       try {
-        await updateWorkerOrderStatus(workerId, 'FINISHED');
+        await updateWorkerOrderStatus(workerId, "FINISHED");
         if (latestContract) {
-          setLatestContract({ ...latestContract, statusOrder: 'FINISHED' });
+          setLatestContract({ ...latestContract, statusOrder: "FINISHED" });
         }
         setShowModal(false);
       } catch (error) {
-        console.error('Error updating contract status', error);
+        console.error("Error updating contract status", error);
       }
     }
   };
@@ -99,9 +130,31 @@ export function PageWorkerIndex() {
     setZoom(zoom === image ? null : image);
   };
 
+  const renderRatingStars = (rating: number) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<FaStar key={i} color="gold" />);
+      } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
+        stars.push(<FaStarHalfAlt key={i} color="gold" />);
+      } else {
+        stars.push(<FaRegStar key={i} color="gold" />);
+      }
+    }
+    return stars;
+  };
+
   return (
     <>
-      <div className="Backg" style={{ backgroundImage: 'url(/b1.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', overflow: 'auto' }}>
+      <div
+        className="Backg"
+        style={{
+          backgroundImage: "url(/b1.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          overflow: "auto",
+        }}
+      >
         <NavbarWComponent />
         {worker && <h1>Willkommen, {worker.name}!</h1>}
         <Container className="mt-0">
@@ -111,42 +164,58 @@ export function PageWorkerIndex() {
                 <Card.Body>
                   <Card.Title className="indexcard2">Nachrichten</Card.Title>
                   <Card.Text>
-                    Bleiben Sie auf dem Laufenden mit den neuesten Nachrichten und Ankündigungen.
+                    <p>Aktuelle bewertung: {renderRatingStars(worker.rating)}</p>
+                    <p>Meist ausgeführter job: {}</p>
+                    <p>JobTypen ausgeführt:{job?.join(", ")}</p>
+                    <p>Durschnitt von min payment:{geld}</p>
+                    <p>Wieviele jobs hatte er: {workerJobAnzahl?.length}</p>
                   </Card.Text>
                 </Card.Body>
               </div>
             </div>
 
-
             {/* Top Right Container */}
             <div className="right-container">
-            <div className="unique-container2">
-              <Card.Body>
-                <Card.Title className="indexcard2">Aktive Aufträge</Card.Title>
-                <Card.Text>
-                {latestContract && latestContract.statusOrder !== 'FINISHED' ? (
+              <div className="unique-container2">
+                <Card.Body>
+                  <Card.Title className="indexcard2">
+                    Aktive Aufträge
+                  </Card.Title>
+                  <Card.Text>
+                    {latestContract &&
+                    latestContract.statusOrder !== "FINISHED" ? (
                       <>
-                        <strong>ID:</strong> {latestContract.id}<br />
-                        <strong>Adresse:</strong> {latestContract.adress || 'Keine Adresse'}<br />
-                        <strong>Job-Typ:</strong> {latestContract.jobType}<br />
-                        <strong>Status:</strong> {latestContract.statusOrder}<br />
-                        <MDBBtn 
-                        onClick={() => navigate(`/worker/${workerId}/order/${latestContract.id}`)}>
-                        Zum Auftrag
-                    </MDBBtn >
+                        <strong>ID:</strong> {latestContract.id}
+                        <br />
+                        <strong>Adresse:</strong>{" "}
+                        {latestContract.adress || "Keine Adresse"}
+                        <br />
+                        <strong>Job-Typ:</strong> {latestContract.jobType}
+                        <br />
+                        <strong>Status:</strong> {latestContract.statusOrder}
+                        <br />
+                        <MDBBtn
+                          onClick={() =>
+                            navigate(
+                              `/worker/${workerId}/order/${latestContract.id}`
+                            )
+                          }
+                        >
+                          Zum Auftrag
+                        </MDBBtn>
                         {/* <Button onClick={handleShowModal} className='anzeigen'>
                           Letzten Vertrag anzeigen
                         </Button> */}
                       </>
                     ) : (
-                      'Gerade hast du noch keine Aufträge.'
+                      "Gerade hast du noch keine Aufträge."
                     )}
-                </Card.Text>
-              </Card.Body>
-            </div>
+                  </Card.Text>
+                </Card.Body>
+              </div>
 
-            {/* Bottom Right Container */}
-            {/* <div className="unique-container2">
+              {/* Bottom Right Container */}
+              {/* <div className="unique-container2">
               <Card.Body>
                 <Card.Title className="indexcard2">Verkehr</Card.Title>
                 <Card.Text>
@@ -154,9 +223,7 @@ export function PageWorkerIndex() {
                 </Card.Text>
               </Card.Body>
             </div> */}
-
-
-          </div>
+            </div>
           </div>
 
           {/* MODAL FÜR EINEN NOCH NICHT ABGESCHLOSSENEN AUFTRAG */}
@@ -205,12 +272,9 @@ export function PageWorkerIndex() {
         </Modal.Footer>
       </Modal> */}
 
-
       <Footer></Footer>
-
     </>
   );
-
 }
 
 export default PageWorkerIndex;
